c16fb892d1e30dbd635a9c1dd1ed61bbd8ead187
Kaylalo <93191169+Kaylalo@users.noreply.github.com>
2022-01-15 22:20:49 +0000

    updated src

---
+++ src/component/data.js
@@ -0,0 +1,23 @@
+const data = {
+    products: [
+     {
+        id: "1",
+        name:"Eat",
+        price:"£12.09",
+        image:"#",
+    },
+    {
+        id: "2",
+        name:"Paranormal Activity 2",
+        price:"£9.65",
+        image:"#",
+    },
+    {
+        id: "3",
+        name:"Thieves (Voleurs, Les)",
+        price:"£2.22",
+        image:"https://www.bing.com/th?id=AMMS_ea10a047596df1fced22a3e78e29a0d7&w=100&h=150&c=7&rs=1&qlt=80&pcl=f9f9f9&o=6&cdv=1&dpr=1.5&pid=16.1",
+        },
+    ],
+};
+export default data; 
